9e7530052e45a12e6571b44369c981a9c71d73c3
Scott Newton <nazcothol@mac.com>
2019-07-15 12:16:58 -0500

    comment out JS and add link colour

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ if a funct starts with cap it's known as a function constructor.
 % math operator that returns just the remainder
 use template literals instead of just + to concatenate
 */
-const name = prompt('Whaat is your name?');
+/* const name = prompt('Whaat is your name?');
 
 document.querySelector('h1').textContent = (`Hello ${name}`);
 // console.log(Boolean(name));
 console.log(x === 5, x <= 5, x >= 5);
-
+*/
