bf9bac39d337dc3812c96363bf6d62d1d8a4c331
Scott Newton <nazcothol@mac.com>
2019-07-14 22:07:30 -0500

    fix footer email link

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 /*
-
+loop, comparison operators to check if val of var is empty or not
+if a funct starts with cap it's known as a function constructor.
+= is assignment operator, name === "Steve" is comparison operator
+% math operator that returns just the remainder
+use template literals instead of just + to concatenate
 */
 const name = prompt('Whaat is your name?');
 
 document.querySelector('h1').textContent = (`Hello ${name}`);
+// console.log(Boolean(name));
+console.log(x === 5, x <= 5, x >= 5);
+
