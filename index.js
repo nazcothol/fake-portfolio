ad3006b604420d8c68be89fe18691ede911bc493
Scott Newton <nazcothol@mac.com>
2019-07-29 19:02:21 -0500

    add script

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ while(!lname){
     lname = prompt('No really, What is it?');
 }
 document.querySelector('h1').textContent = (`Hello ${fname} ${lname}`);*/
+// bring in the functional components
 import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -36,6 +37,8 @@ const states = {
 
 
 function render(state){
+    console.log('state came in as:', state);
+
     // we use function invocation that actually runs the fnctn and then 'returns' the mkup so it's prop browser rendered
     document.querySelector('#root').innerHTML = `
     ${Navigation(state)}
@@ -45,31 +48,14 @@ function render(state){
     `;
 }
 render(states.home);
-
+//  POS = Piece of State Oo
+// new below
 const navItems = document.querySelectorAll('nav>ul>li:not(.dropdown)');
 
-let i = 0;
-// here i will run from 0 until 3.
-// 'i' will serve as a placeholder to represent the index of the item we need to access from navItems
-
-while(i < navItems.length){
-    // use val of i as an index to access the navItem.
-    navItems[i].addEventListener('click', function clickHandler(event){
+navItems.forEach(function eventListenerAdder(navItem){
+    navItem.addEventListener('click', function clickHandler(event){
         event.preventDefault();
-        // 'target will reveal what generated the event - that is, what was'clicked'
 
-        const clickedItem = event.target;
-        const clicked = clickedItem.toLowerCase();
-
-        render(states[event.target.textcontent]);
+        render(states[event.target.textContent.toLowerCase()]);
     });
-    i += 1;
-}
-
-
-/* We want to re-render page based on what user clicks in navigation menu
-1. capture what was clicked in nav - what is the text of the element that got the click
-2. access a piece of state based on said text
-3. show the new vue **render function**
-4.5. re-render page with new piece of state that is derived from what was clicked
-*/
+});
