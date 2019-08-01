ddd093197cc3cdd93c8fb3be97253a7ee5825755
Scott Newton <nazcothol@mac.com>
2019-07-31 20:43:33 -0500

    update store with html

--- index.js
+++ index.js
@@ -22,16 +22,37 @@ import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
+// holds the pieces of State down there
 const store = {
-    'home': {
-        'title': 'Learnings'
+    'links': {
+        'primary': [ 'Home', 'About', 'Contact' ],
+        'dropdown': [ 'Project 1', 'Project 2', 'Project 3' ]
     },
+    'home': {
+        'title': 'Learnings',
+        'page': `
+            <main>
+
+            <section class="float">
+                <h2>Mission</h2>
+                <p>This site may not be as slick as <a href="https://pageandsound.com/"><text x="10" y="25">Page and Sound</text></a>, but there is a reason for that. The goal for this site is to serve as a playground to show off and try all the techie things I'm learning. Everything here is made from scratch. No WordPress, just straight HTML, CSS, Javascript, and other bare-bones technologies that I'm learning. Projects shared here may be rough, but that's how learnings work.
+            <a href="#" class="cta-btn">Read More!</a>
+            </section>
+            <section>
+                    <h2>This is a section</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid atque, impedit, distinctio exercitationem omnis esse hic quisquam magni quis fuga qui vel consequatur natus itaque iusto, fugit unde deserunt.</p>
+                    <a href="#" class="cta-btn">Read More!</a>
+                </section>
+            </main>`
+    },
+
     'about': {
-        'title': 'About Page'
+        'title': 'About Page',
+        'page': ''
     },
     'contact': {
-        'title': 'Contact'
+        'title': 'Contact',
+        'page': ''
     }
 };
 
@@ -44,7 +65,8 @@ function render(state){
     ${Main(state)}
     ${Footer(state)}
     `;
-
+    // nav functional component renders 'new' links each time
+    // the elements won't exist until pages are rendered
     const navItems = document.querySelectorAll('nav>ul>li:not(.dropdown)');
 
     navItems.forEach(function eventListenerAdder(navItem){
